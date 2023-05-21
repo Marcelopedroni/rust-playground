25a59872a2cb3fe02e74e8d90a3b8c691cdb8c54
marcelopedroni <marcelopedronidasilva@gmail.com>
2023-05-21 15:06:35 -0300

    added loop iteration until the correct answer is given

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,27 @@ use std::io;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut guesses = 1;
+    // iterate over and over until the guess is correct
+    loop {
+        println!("Please enter your guess number (between 1 - 100): ");
+        let mut guess_number = String::new();
 
-    println!("Please enter your guess number: ");
+        io::stdin()
+            .read_line(&mut guess_number)
+            .expect("Failed to read line");
 
-    let mut guess_number = String::new();
+        let guess_number: usize = guess_number
+            .trim()
+            .parse()
+            .expect("Your guess was not a number");
 
-    io::stdin()
-        .read_line(&mut guess_number)
-        .expect("Failed to read line");
-
-    let guess_number: usize = guess_number
-        .trim()
-        .parse()
-        .expect("Your guess was not a number");
-
-    match guess_number.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("YOU DID IT! Congratulations!!"),
+        match guess_number.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => break
+        }
+        guesses += 1;
     }
-
-    println!("The secret number is {secret_number}");
+    println!("Congrats, you guessed the secret number => {secret_number}. You took {guesses} guesses to discover!");
 }
